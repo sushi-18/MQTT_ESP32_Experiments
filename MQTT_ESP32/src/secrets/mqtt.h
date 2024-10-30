b12b14774044b174e9fb7b07a1f8be1adfff5e72
sushi-18 <116638632+sushi-18@users.noreply.github.com>
2024-10-31 04:05:51 +0700

    Delete MQTT_ESP32 directory

--- MQTT_ESP32/src/secrets/mqtt.h
+++
@@ -1,7 +0,0 @@
-namespace MQTT
-{
-    const char *username = "sonnguyen";
-    const char *password = "18032003";
-    const char *broker = "fac8405da1f54591a0881bbb1d123015.s1.eu.hivemq.cloud";
-    uint16_t port = 8883;
-}
--- MQTT_ESP32/src/secrets/wifi.h
+++
@@ -1,9 +0,0 @@
-namespace WiFiSecrets
-{
-    const char *ssid = "Phan Diep";
-    const char *pass = "0948951473";
-    const char *echo_topic = "esp32/echo_test";
-    unsigned int publish_count = 0;
-    uint16_t keepAlive = 15;
-    uint16_t socketTimeout = 5;
-}
