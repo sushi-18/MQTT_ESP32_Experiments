b996f9c264e11ef64750464423ac3c05a4795149
sushi-18 <116638632+sushi-18@users.noreply.github.com>
2024-10-31 04:07:09 +0700

    Add files via upload

---
+++ MQTT_ESP32/src/secrets/mqtt.h
@@ -0,0 +1,7 @@
+namespace MQTT
+{
+    const char *username = "sonnguyen";
+    const char *password = "18032003";
+    const char *broker = "fac8405da1f54591a0881bbb1d123015.s1.eu.hivemq.cloud";
+    uint16_t port = 8883;
+}
---
+++ MQTT_ESP32/src/secrets/wifi.h
@@ -0,0 +1,9 @@
+namespace WiFiSecrets
+{
+    const char *ssid = "sonnguyen";
+    const char *pass = "18032003";
+    const char *echo_topic = "esp32/echo_test";
+    unsigned int publish_count = 0;
+    uint16_t keepAlive = 15;
+    uint16_t socketTimeout = 5;
+}
