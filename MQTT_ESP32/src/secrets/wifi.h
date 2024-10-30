2cdd510316467f91716f75cc2b8c1c89df7a1d4d
sushi-18 <116638632+sushi-18@users.noreply.github.com>
2024-10-31 01:20:56 +0700

    Add files via upload

---
+++ MQTT_ESP32/src/secrets/wifi.h
@@ -0,0 +1,9 @@
+namespace WiFiSecrets
+{
+    const char *ssid = "Phan Diep";
+    const char *pass = "0948951473";
+    const char *echo_topic = "esp32/echo_test";
+    unsigned int publish_count = 0;
+    uint16_t keepAlive = 15;
+    uint16_t socketTimeout = 5;
+}
